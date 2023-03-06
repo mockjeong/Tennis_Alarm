a69a88ec14fb755ff505c8c3b77197b7390045af
Jeongmock Yoo <jeongmock@gmail.com>
2023-03-06 18:38:55 +0900

    3/6 Dayjs 적용

--- routes/tennis.js
+++ routes/tennis.js
@@ -7,15 +7,35 @@ const {spoCheck} = require('../reserves/SP_PP.js');
 var fs = require('fs');
 const handlebars = require('handlebars');
 
+//Import for Timecal
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+const isSameOrAfter = require('dayjs/plugin/isSameOrAfter');
+const isSameOrBefore = require('dayjs/plugin/isSameOrBefore');
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(isSameOrAfter);
+dayjs.extend(isSameOrBefore);
+
 router.use('/', async (req, res) =>{
-const { month, day } = req.body;
+  const { month, day } = req.body;
+
+  //TargetMonth, Day to string with 0 (use padStart)
+  let inputDayStr = day.toString().padStart(2, '0');
+
+  const today = dayjs().tz("Asia/Seoul").startOf('day'); // Get the start of today in the client's timezone
+  const inputDate = dayjs(`${month}-${inputDayStr}`).startOf('day'); // Get the start of the target date
+
+  const isWithin7Days = inputDate.isSameOrAfter(today) && inputDate.isSameOrBefore(today.add(6, 'day'));
+  // console.log(isWithin7Days);
 
   // Check if the input month and day are within 7 days of today's date
-  const inputDate = new Date(`${month}-${day}`);
-  const today = new Date();
-  const oneWeekLater = new Date(today.getTime() + (7 * 24 * 60 * 60 * 1000));
+  // const inputDate = new Date(`${month}-${day}`);
+  // const today = new Date();
+  // const oneWeekLater = new Date(today.getTime() + (7 * 24 * 60 * 60 * 1000));
   
-  if (inputDate < today || inputDate > oneWeekLater) {
+  if (!(isWithin7Days)) {
     // Show an error message to the user
     req.flash('error', '오늘부터 7일 이내로 설정해주세요.');
     res.redirect('/');
@@ -41,7 +61,7 @@ const { month, day } = req.body;
       // Check if there were any rejected promises
       if (rejectedResults.length > 0) {
         const errorMessages = rejectedResults.map(result => result.reason.message);
-        // req.flash('error', errorMessages.join('\n'));
+        //  req.flash('error', errorMessages.join('\n'));
         req.flash('error', '오류가 발생했습니다. 다시 조회 해주시기 바랍니다.');
         res.redirect('/');
         return;
